065b573bca95be0c4e19c044a65be56db857b0bb
jordsek <73316853+jordsek@users.noreply.github.com>
2023-11-22 19:49:15 +0000

    first commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Jordan Isac on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
